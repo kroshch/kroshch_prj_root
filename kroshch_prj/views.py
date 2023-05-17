d0ea00eb579371dc7a1e6a33fbf63bed46264ba6
kroshch 47 <kroshch3228@gmail.com>
2023-05-17 19:30:26 +0300

    Новий стан, за якого додано параграф до файлу home.html

---
+++ kroshch_prj/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+	return HttpResponse('Це інформація про проєкт')
+
+def home(request):
+	return render(request, 'home.html',  {'greeting':'Доброго дня'})
+
